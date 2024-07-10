5d29fce51eb2f21d1803d7c078b9541de5c5b7a6
Shubham Bhilare <140585267+Skb3611@users.noreply.github.com>
2024-07-11 02:58:49 +0530

    Update ApiRoutes.js

--- app/api/ApiRoutes.js
+++ app/api/ApiRoutes.js
@@ -1,4 +1,4 @@
-let host ="http://localhost:3000/"
+let host ="https://focus-hub-xi.vercel.app/"
 export const SignupApi=`${host}api/signup`
 export const LoginApi=`${host}api/login`
 export const EnquiryApi=`${host}api/enquiry`
