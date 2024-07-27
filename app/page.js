ea6d2c28c63110e7dea3f92a09d07f53471806fd
SKB <shubhambhilare4899223@gmail.com>
2024-07-28 02:35:37 +0530

    suspense

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Slider from "@/components/Slider";
 import About from "@/components/About";
@@ -6,57 +6,57 @@ import Slot from "@/components/Slot";
 import Courses from "@/components/Courses";
 import { useSearchParams } from "next/navigation";
 import { host, Paymentsuccess } from "./api/ApiRoutes";
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
-import { Suspense } from "react";
-export default function Home() {
-  const router=useRouter()
-  let toastoptions = {
-    position: "top-right",
-    autoClose: 1000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "dark",
-  };
-  let a = useSearchParams()
-  if(a.size>0){
-    a= a.get("payment")
-  
-    if(a){
-      let token = JSON.parse(localStorage.getItem("token"));
-      let decoded = jwt.decode(token);
-      let func=async () => {
-        let a=await fetch(Paymentsuccess,{
-          method:"POST",
-          headers:{
-            "Content-Type":"application/json"
-          },
-          body:JSON.stringify({email:decoded.email})
-        })
-         a=await a.json()
-        
-         if(a.success){
-          toast.success(a.message,toastoptions)
-          router.push(host)
-         }
-      }
-      func()
-    }
-  }
+import { Suspense, useEffect } from "react";
 
+export default function Home() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   
+  useEffect(() => {
+    const handlePaymentSuccess = async () => {
+      const payment = searchParams.get("payment");
+      if (payment) {
+        const token = JSON.parse(localStorage.getItem("token"));
+        const decoded = jwt.decode(token);
+        const response = await fetch(Paymentsuccess, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email: decoded.email }),
+        });
+        const result = await response.json();
+        
+        if (result.success) {
+          toast.success(result.message, {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          });
+          router.push(host);
+        }
+      }
+    };
+
+    handlePaymentSuccess();
+  }, [searchParams, router]);
+
   return (
-    <Suspense fallback={<></>}>
-   <div className="scroll-smooth ">
-   <Slider/>
-   <Slot/>
-   <Courses/>
-   <About/>
-   </div>
+    <Suspense fallback={<div> </div>}>
+      <div className="scroll-smooth">
+        <Slider />
+        <Slot />
+        <Courses />
+        <About />
+      </div>
     </Suspense>
   );
 }
