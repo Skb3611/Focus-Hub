1212883e819e29fcf7eeacd4a0306f363491306d
SKB <shubhambhilare4899223@gmail.com>
2024-07-28 22:55:11 +0530

    netlify

--- app/api/razorpay/route.js
+++ app/api/razorpay/route.js
@@ -8,13 +8,28 @@ export async function POST(request) {
     let body = await request.formData();
     body = Object.fromEntries(body);
 
- 
+    let p = await payment.findOne({ oid: body.razorpay_order_id });
+    console.log(p);
+    if (!p)
+      return NextResponse.json({ success: false, message: "No payment found" });
+
+    let verify = validatePaymentVerification(
+      {
+        order_id: body.razorpay_order_id,
+        payment_id: body.razorpay_payment_id,
+      },
+      body.razorpay_signature,
+      process.env.RAZORPAY_SECRET
+    );
+    if (!verify)
+      return NextResponse.json({ success: false, message: "Invalid Id's" });
     const updatedpayment = await payment.findOneAndUpdate(
       { oid: body.razorpay_order_id },
-      { status: true }
+      { status: true },
+      { new: true }
     );
     return NextResponse.redirect(
-      `${host}/courses/${updatedpayment.category}/${updatedpayment.CourseName}?payment=true`
+      `${host}/courses/${p.category}/${p.CourseName}?payment=true`
     );
   } catch (error) {
     console.log(error);
